bdacf7fad0a9b74f796ffa931b508b81050eba20
new92 <94779840+new92@users.noreply.github.com>
2023-01-15 14:34:30 +0200

    Update mainV2.py

--- InstagramFollowers/V2/mainV2.py
+++ InstagramFollowers/V2/mainV2.py
@@ -1,9 +1,13 @@
 """
 Author: new92
 Github: @new92
-InstaFollowV2: Is a program similar to Version 1 (V1) but I found a different way to build the same program with fewer lines of code.
+InstaFollowV2: Script for increasing the followers of an Instagram account
+
+*********IMPORTANT*********
+
 User's data (such as: username, password) will not be stored or saved ! 
-Will be used only to increase the followers of the user's account
+Will be used only to increase the followers of the user's Instagram account
+***************************
 """
 
 try:
@@ -13,7 +17,6 @@ try:
     from time import sleep
     import instagrapi
     import os
-    import requests
     import instabot
     import instapy
     import instaloader
@@ -48,16 +51,18 @@ except ImportError as imp:
         system("pip install -r requirements.txt")
 
 
-print("""
+banner = """
 ██╗███╗░░██╗░██████╗████████╗░█████╗░███████╗░█████╗░██╗░░░░░██╗░░░░░░█████╗░░██╗░░░░░░░██╗    ██╗░░░██╗██████╗░
 ██║████╗░██║██╔════╝╚══██╔══╝██╔══██╗██╔════╝██╔══██╗██║░░░░░██║░░░░░██╔══██╗░██║░░██╗░░██║    ██║░░░██║╚════██╗
 ██║██╔██╗██║╚█████╗░░░░██║░░░███████║█████╗░░██║░░██║██║░░░░░██║░░░░░██║░░██║░╚██╗████╗██╔╝    ╚██╗░██╔╝░░███╔═╝
 ██║██║╚████║░╚═══██╗░░░██║░░░██╔══██║██╔══╝░░██║░░██║██║░░░░░██║░░░░░██║░░██║░░████╔═████║░    ░╚████╔╝░██╔══╝░░
 ██║██║░╚███║██████╔╝░░░██║░░░██║░░██║██║░░░░░╚█████╔╝███████╗███████╗╚█████╔╝░░╚██╔╝░╚██╔╝░    ░░╚██╔╝░░███████╗
 ╚═╝╚═╝░░╚══╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░░░░░╚════╝░╚══════╝╚══════╝░╚════╝░░░░╚═╝░░░╚═╝░░    ░░░╚═╝░░░╚══════╝
-""")
+"""
+print(banner)
+print("\t\tScript by @new92")
 print("\n")
-print("[+] Program for increasing followers on Instagram")
+print("[+] Program for increasing the followers of an account on Instagram (works for both public and private accounts)")
 print("\n")
 print("[+] Author: new92")
 print("[+] Github: @new92")
@@ -73,33 +78,12 @@ while option < 1 or option > 2 or option == None:
 if option == 1:
     print("[+] The data will not be stored or saved")
     sleep(2)
-    print("-"*40)
-    print("login".upper())
+    print("|"+"-"*20+"login".upper()+"-"*20+"|")
     username=str(input("[::] Please enter your username: "))
     while username == None or len(username) > 30:
         print("[!] Invalid username !")
         sleep(1)
         username=str(input("[::] Please enter again your username: "))
-    resp = requests.get(f"https://www.instagram.com/{username}/")
-    while resp.status_code == 404 or resp.status_code == 400:
-        print("[!] User not found !")
-        sleep(1)
-        print("[1] Try with another username")
-        print("[2] Exit")
-        opt=int(input(">>> "))
-        while opt < 1 or opt > 2 or opt == None:
-            print("[!] Invalid number !")
-            sleep(1)
-            opt=int(input(">>> "))
-        if opt == 1:
-            username=str(input("[::] Please enter the username: "))
-            while username == None:
-                print("[!] Invalid username !")
-                sleep(1)
-                username=str(input("[::] Please enter the username: "))
-        else:
-            print("[+] Exiting...")
-            quit(0)
     username=username.lower()
     username=username.strip()
     sleep(1)
@@ -110,10 +94,11 @@ if option == 1:
         password=input("[::] Please enter again your password: ")
     password=password.strip()
     sleep(1)
+    print("|"+"-"*45+"|")
     clnt=instagrapi.Client()
     try:
         login = clnt.login(username,password)
-        if login == True:
+        if login:
             print("[!] Login successful !")
             sleep(2)
             print("[+] Please wait while the program is increasing your followers...")
@@ -136,11 +121,11 @@ if option == 1:
     sleep(2)
     temp = True
     FOLLOW=['173560420','1436859892','18428658','7719696','451573056','247944034','407964088','460563723','6860189','427553890','26669533','4213518589','12331195','28527810','12281817','208560325','145821237','305701719','217867189','20824486','25025320','787132','260375673','290023231','1269788896','29394004','11830955','6380930','2094200507','9777455']
-    UNFOLLOW=['173560420','1436859892','18428658','7719696','451573056','247944034','407964088','460563723','6860189','427553890','26669533','4213518589','12331195','28527810','12281817','208560325','145821237','305701719','217867189','20824486','25025320','787132','260375673','290023231','1269788896','29394004','11830955','6380930','2094200507','9777455']
+    NAMES = ['Cristiano Ronaldo','Cardi B','Kim Kardashian','Ariana Grande','Nicki Minaj','Beyonce','Katy Perry','Selena Gomez','Justin Bieber','Lionel Messi','Neymar Jr','Kylian Mbappe','Dua Lipa','Billie Eilish','Kylie Jenner','Khloe Kardashian','Kourtney Kardashian','Jennifer Lopez','Shakira','NBA','Instagram','National Geographic','FC Barcelona','Real Madrid','Champions League','Chris Brown','Taylor Swift','Kendall Jenner','Virat Kohli','Zendaya']
     print("[!] NOTE: Use this program every 2 days in order for your account not to get blocked")
     sleep(5)
     while temp:
-        for i in range(len(FOLLOW)):
+        for i in range(len(FOLLOW)-1):
             try:
                 clnt.user_follow(FOLLOW[i])
             except Exception as key:
@@ -150,11 +135,13 @@ if option == 1:
                 sleep(1)
                 print("[+] Exiting...")
                 quit(0)
-            print(f"[+] Following {FOLLOW[i]}...")
+            print(f"[+] Following {NAMES[i]}...")
+            sleep(3)
+            print(f"[+] Next user to follow: {NAMES[i+1]}")
             sleep(1)
-        for j in range(len(UNFOLLOW)):
+        for i in range(len(FOLLOW)-1):
             try:
-                clnt.user_unfollow(UNFOLLOW[j])
+                clnt.user_unfollow(FOLLOW[i])
             except Exception as ex:
                 print("[!] Error !")
                 sleep(1)
@@ -162,5 +149,7 @@ if option == 1:
                 sleep(1)
                 print("[+] Exiting...")
                 quit(0)
-            print(f"[+] Unfollowing {UNFOLLOW[j]}...")
+            print(f"[+] Unfollowing {NAMES[i]}...")
+            sleep(3)
+            print(f"[+] Next user to unfollow: {NAMES[i+1]}")
             sleep(1)
