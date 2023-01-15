b540163731dee05ee90463fb79d676e0d58a377f
new92 <94779840+new92@users.noreply.github.com>
2023-01-15 13:12:07 +0200

    Update mainV1.py

--- InstagramFollowers/V1/mainV1.py
+++ InstagramFollowers/V1/mainV1.py
@@ -23,8 +23,24 @@ except ImportError as imp:
     print("[+] Ignoring warning...")
     sleep(1)
     if sys.platform.startswith('linux') == True:
-        system("sudo pip install -r requirements.txt")
-        pass
+        if os.geteuid() != 0:
+            print("[!] Root user not detected !")
+            sleep(2)
+            print("[+] Trying to enable root user...")
+            sleep(1)
+            system(sudo su)
+            try:
+                system(sudo pip install -r requirements.txt)
+            except Exception as ex:
+                print("[!] Error !")
+                sleep(1)
+                print(ex)
+                sleep(2)
+                print("[+] Exiting...")
+                quit(0)
+        else:
+            system("sudo pip install -r requirements.txt")
+            pass
     elif sys.platform == 'darwin':
         system("python -m pip install requirements.txt")
         pass
