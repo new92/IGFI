5a7fc8f1a0bd42656e4a6a0cdbe49c2f1b7c7aa8
new92 <94779840+new92@users.noreply.github.com>
2023-02-10 20:52:05 +0200

    Update mainV1.py

--- InstagramFollowers/V1/mainV1.py
+++ InstagramFollowers/V1/mainV1.py
@@ -60,7 +60,7 @@ def ProgInfo():
     else:
         fsize = 0
     stars = 14
-    forks = stars // 2
+    forks = 8
     print("[+] Author: "+str(author))
     print("[+] Github: @"+str(author))
     print("[+] License: "+str(license1))
