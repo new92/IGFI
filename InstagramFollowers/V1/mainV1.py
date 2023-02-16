ba8e00415a4d88538cb0160af3084a7926182d4b
new92 <94779840+new92@users.noreply.github.com>
2023-02-16 10:40:16 +0200

    ✅

--- InstagramFollowers/V1/mainV1.py
+++ InstagramFollowers/V1/mainV1.py
@@ -94,7 +94,7 @@ def ProgInfo():
     lang = 'es-US'
     language = 'Python'
     name = 'InstaFollowV1'
-    lines = 838
+    lines = 844
     f = '/IGFollowersIncreaser/InstagramFollowers/V1/mainV1.py'
     if os.path.exists(os.path.abspath(f)):
         fsize = (os.stat(f)).st_size
@@ -264,15 +264,18 @@ def main():
                 print("[3] Exit")
                 opt=int(input("[::] Please enter again a number (from the above ones): "))
             if opt == 1:
-                if username == None:
-                    print("[!] This field can't be blank !")
-                else:
-                    print("[!] Invalid username !")
+                username=str(input("[::] Please enter the username: "))
+                while checkUser(username):
+                    if username == None:
+                        print("[!] This field can't be blank !")
+                    else:
+                        print("[!] Invalid username !")
+                        sleep(1)
+                        print("[+] The length of the username must be lower than or equal to 30 characters.")
                     sleep(1)
-                    print("[+] The length of the username must be lower than or equal to 30 characters.")
-                sleep(1)
-                username=str(input("[::] Please enter again the username: "))
+                    username=str(input("[::] Please enter again the username: "))
             elif opt == 2:
+                break
                 main()
             else:
                 print("[+] Thank you for using my script 😁")
@@ -297,16 +300,19 @@ def main():
             followers_bef = user.number_of_followers
         sleep(1)
         password=str(input("[::] Please enter your password: "))
-        while password == None:
-            print("[!] This field can't be blank !")
+        while password == None or type(password) != str:
+            if password == None:
+                print("[!] This field can't be blank !")
+            else:
+                print("[!] Password must be a string !")
             sleep(1)
             password=str(input("[::] Please enter again your password: "))
         password=password.strip()
         sleep(1)
         print("|"+"-"*45+"|")
-        clnt=instagrapi.Client()
+        client=instagrapi.Client()
         try:
-            login = clnt.login(username,password)
+            login = client.login(username,password)
             if login:
                 print("[!] Login successful !")
                 sleep(2)
@@ -373,243 +379,243 @@ def main():
         unfollow = 0
         while True:
             try:
-                clnt.user_follow(173560420) #Cristiano Ronaldo
+                client.user_follow(173560420) #Cristiano Ronaldo
                 follow += 1
                 print("[+] Following 173560420...")
                 sleep(2)
-                clnt.user_follow(1436859892) #Cardi B
+                client.user_follow(1436859892) #Cardi B
                 follow += 1
                 print("[+] Following 1436859892...")
                 sleep(2)
-                clnt.user_follow(18428658) #Kim Kardashian
+                client.user_follow(18428658) #Kim Kardashian
                 follow += 1
                 print("[+] Following 18428658...")
                 sleep(2)
-                clnt.user_follow(7719696) #Ariana Grande
+                client.user_follow(7719696) #Ariana Grande
                 follow += 1
                 print("[+] Following 7719696...")
                 sleep(2)
-                clnt.user_follow(451573056) #Nicki Minaj
+                client.user_follow(451573056) #Nicki Minaj
                 follow += 1
                 print("[+] Following 451573056...")
                 sleep(2)
-                clnt.user_follow(247944034) #Beyonce
+                client.user_follow(247944034) #Beyonce
                 follow += 1
                 print("[+] Following 247944034...")
                 sleep(2)
-                clnt.user_follow(407964088) #Katy Perry
+                client.user_follow(407964088) #Katy Perry
                 follow += 1
                 print("[+] Following 407964088...")
                 sleep(2)
-                clnt.user_follow(460563723) #Selena Gomez
+                client.user_follow(460563723) #Selena Gomez
                 follow += 1
                 print("[+] Following 460563723...")
                 sleep(2)
-                clnt.user_follow(6860189) #Justin Bieber
+                client.user_follow(6860189) #Justin Bieber
                 follow += 1
                 print("[+] Following 6860189...")
                 sleep(2)
-                clnt.user_follow(427553890) #Lionel Messi
+                client.user_follow(427553890) #Lionel Messi
                 follow += 1
                 print("[+] Following 427553890...")
                 sleep(2)
-                clnt.user_follow(26669533) #Neymar Jr
+                client.user_follow(26669533) #Neymar Jr
                 follow += 1
                 print("[+] Following 26669533...")
                 sleep(2)
-                clnt.user_follow(4213518589) #Kylian Mbappe
+                client.user_follow(4213518589) #Kylian Mbappe
                 follow += 1
                 print("[+] Following 4213518589...")
                 sleep(2)
-                clnt.user_follow(12331195) #Dua Lipa
+                client.user_follow(12331195) #Dua Lipa
                 follow += 1
                 print("[+] Following 12331195...")
                 sleep(2)
-                clnt.user_follow(28527810) #Billie Eilish
+                client.user_follow(28527810) #Billie Eilish
                 follow += 1
                 print("[+] Following 28527810...")
                 sleep(2)
-                clnt.user_follow(12281817) #Kylie Jenner
+                client.user_follow(12281817) #Kylie Jenner
                 follow += 1
                 print("[+] Following 12281817...")
                 sleep(2)
-                clnt.user_follow(208560325) #Khloe Kardashian
+                client.user_follow(208560325) #Khloe Kardashian
                 follow += 1
                 print("[+] Following 208560325...")
                 sleep(2)
-                clnt.user_follow(145821237) #Kourtney Kardashian
+                client.user_follow(145821237) #Kourtney Kardashian
                 follow += 1
                 print("[+] Following 145821237...")
                 sleep(2)
-                clnt.user_follow(305701719) #Jennifer Lopez
+                client.user_follow(305701719) #Jennifer Lopez
                 follow += 1
                 print("[+] Following 305701719...")
                 sleep(2)
-                clnt.user_follow(217867189) #Shakira
+                client.user_follow(217867189) #Shakira
                 follow += 1
                 print("[+] Following 217867189...")
                 sleep(2)
-                clnt.user_follow(20824486) #NBA
+                client.user_follow(20824486) #NBA
                 follow += 1
                 print("[+] Following 20824486...")
                 sleep(2)
-                clnt.user_follow(25025320) #Instagram
+                client.user_follow(25025320) #Instagram
                 follow += 1
                 print("[+] Following 25025320...")
                 sleep(2)
-                clnt.user_follow(787132) #National Geographic
+                client.user_follow(787132) #National Geographic
                 follow += 1
                 print("[+] Following 787132...")
                 sleep(2)
-                clnt.user_follow(260375673) #FC Barcelona
+                client.user_follow(260375673) #FC Barcelona
                 follow += 1
                 print("[+] Following 260375673...")
                 sleep(2)
-                clnt.user_follow(290023231) #Real Madrid
+                client.user_follow(290023231) #Real Madrid
                 follow += 1
                 print("[+] Following 290023231...")
                 sleep(2)
-                clnt.user_follow(1269788896) #Champions League
+                client.user_follow(1269788896) #Champions League
                 follow += 1
                 print("[+] Following 1269788896...")
                 sleep(2)
-                clnt.user_follow(29394004) #Chris Brown
+                client.user_follow(29394004) #Chris Brown
                 follow += 1
                 print("[+] Following 29394004...")
                 sleep(2)
-                clnt.user_follow(11830955) #Taylor Swift
+                client.user_follow(11830955) #Taylor Swift
                 follow += 1
                 print("[+] Following 11830955...")
                 sleep(2)
-                clnt.user_follow(6380930) #Kendall Jenner
+                client.user_follow(6380930) #Kendall Jenner
                 follow += 1
                 print("[+] Following 6380930...")
                 sleep(2)
-                clnt.user_follow(2094200507) #Virat Kohli
+                client.user_follow(2094200507) #Virat Kohli
                 follow += 1
                 print("[+] Following 2094200507...")
                 sleep(2)
-                clnt.user_follow(9777455) #Zendaya
+                client.user_follow(9777455) #Zendaya
                 follow += 1
                 print("[+] Following 9777455...")
                 sleep(2)
-                clnt.user_unfollow(173560420) #Cristiano Ronaldo
+                client.user_unfollow(173560420) #Cristiano Ronaldo
                 unfollow += 1
                 print("[+] Unfollowing 173560420...")
                 sleep(2)
-                clnt.user_unfollow(1436859892) #Cardi B
+                client.user_unfollow(1436859892) #Cardi B
                 unfollow += 1
                 print("[+] Unfollowing 1436859892...")
                 sleep(2)
-                clnt.user_unfollow(18428658) #Kim Kardashian
+                client.user_unfollow(18428658) #Kim Kardashian
                 unfollow += 1
                 print("[+] Unfollowing 18428658...")
                 sleep(2)
-                clnt.user_unfollow(7719696) #Ariana Grande
+                client.user_unfollow(7719696) #Ariana Grande
                 unfollow += 1
                 print("[+] Unfollowing 7719696...")
                 sleep(2)
-                clnt.user_unfollow(451573056) #Nicki Minaj
+                client.user_unfollow(451573056) #Nicki Minaj
                 unfollow += 1
                 print("[+] Unfollowing 451573056...")
                 sleep(2)
-                clnt.user_unfollow(247944034) #Beyonce
+                client.user_unfollow(247944034) #Beyonce
                 unfollow += 1
                 print("[+] Unfollowing 247944034...")
                 sleep(2)
-                clnt.user_unfollow(407964088) #Katy Perry
+                client.user_unfollow(407964088) #Katy Perry
                 unfollow += 1
                 print("[+] Unfollowing 407964088...")
                 sleep(2)
-                clnt.user_unfollow(460563723) #Selena Gomez
+                client.user_unfollow(460563723) #Selena Gomez
                 unfollow += 1
                 print("[+] Unfollowing 460563723...")
                 sleep(2)
-                clnt.user_unfollow(6860189) #Justin Bieber
+                client.user_unfollow(6860189) #Justin Bieber
                 unfollow += 1
                 print("[+] Unfollowing 6860189...")
                 sleep(2)
-                clnt.user_unfollow(427553890) #Lionel Messi
+                client.user_unfollow(427553890) #Lionel Messi
                 unfollow += 1
                 print("[+] Unfollowing 427553890...")
                 sleep(2)
-                clnt.user_unfollow(26669533) #Neymar Jr
+                client.user_unfollow(26669533) #Neymar Jr
                 unfollow += 1
                 print("[+] Unfollowing 26669533...")
                 sleep(2)
-                clnt.user_unfollow(4213518589) #Kylian Mbappe
+                client.user_unfollow(4213518589) #Kylian Mbappe
                 unfollow += 1
                 print("[+] Unfollowing 4213518589...")
                 sleep(2)
-                clnt.user_unfollow(12331195) #Dua Lipa
+                client.user_unfollow(12331195) #Dua Lipa
                 unfollow += 1
                 print("[+] Unfollowing 12331195...")
                 sleep(2)
-                clnt.user_unfollow(28527810) #Billie Eilish
+                client.user_unfollow(28527810) #Billie Eilish
                 unfollow += 1
                 print("[+] Unfollowing 28527810...")
                 sleep(2)
-                clnt.user_unfollow(12281817) #Kylie Jenner
+                client.user_unfollow(12281817) #Kylie Jenner
                 unfollow += 1
                 print("[+] Unfollowing 12281817...")
                 sleep(2)
-                clnt.user_unfollow(208560325) #Khloe Kardashian
+                client.user_unfollow(208560325) #Khloe Kardashian
                 unfollow += 1
                 print("[+] Unfollowing 208560325...")
                 sleep(2)
-                clnt.user_unfollow(145821237) #Kourtney Kardashian
+                client.user_unfollow(145821237) #Kourtney Kardashian
                 unfollow += 1
                 print("[+] Unfollowing 145821237...")
                 sleep(2)
-                clnt.user_unfollow(305701719) #Jennifer Lopez
+                client.user_unfollow(305701719) #Jennifer Lopez
                 unfollow += 1
                 print("[+] Unfollowing 305701719...")
                 sleep(2)
-                clnt.user_unfollow(217867189) #Shakira
+                client.user_unfollow(217867189) #Shakira
                 unfollow += 1
                 print("[+] Unfollowing 217867189...")
                 sleep(2)
-                clnt.user_unfollow(20824486) #NBA
+                client.user_unfollow(20824486) #NBA
                 unfollow += 1
                 print("[+] Unfollowing 20824486...")
                 sleep(2)
-                clnt.user_unfollow(25025320) #Instagram
+                client.user_unfollow(25025320) #Instagram
                 unfollow += 1
                 print("[+] Unfollowing 25025320...")
                 sleep(2)
-                clnt.user_unfollow(787132) #National Geographic
+                client.user_unfollow(787132) #National Geographic
                 unfollow += 1
                 print("[+] Unfollowing 787132...")
                 sleep(2)
-                clnt.user_unfollow(260375673) #FC Barcelona
+                client.user_unfollow(260375673) #FC Barcelona
                 unfollow += 1
                 print("[+] Unfollowing 260375673...")
                 sleep(2)
-                clnt.user_unfollow(290023231) #Real Madrid
+                client.user_unfollow(290023231) #Real Madrid
                 unfollow += 1
                 print("[+] Unfollowing 290023231...")
                 sleep(2)
-                clnt.user_unfollow(1269788896) #Champions League
+                client.user_unfollow(1269788896) #Champions League
                 unfollow += 1
                 print("[+] Unfollowing 1269788896...")
                 sleep(2)
-                clnt.user_unfollow(29394004) #Chris Brown
+                client.user_unfollow(29394004) #Chris Brown
                 unfollow += 1
                 print("[+] Unfollowing 29394004...")
                 sleep(2)
-                clnt.user_unfollow(11830955) #Taylor Swift
+                client.user_unfollow(11830955) #Taylor Swift
                 unfollow += 1
                 print("[+] Unfollowing 11830955...")
                 sleep(2)
-                clnt.user_unfollow(6380930) #Kendall Jenner
+                client.user_unfollow(6380930) #Kendall Jenner
                 unfollow += 1
                 print("[+] Unfollowing 6380930...")
                 sleep(2)
-                clnt.user_unfollow(2094200507) #Virat Kohli
+                client.user_unfollow(2094200507) #Virat Kohli
                 unfollow += 1
                 print("[+] Unfollowing 2094200507...")
                 sleep(2)
-                clnt.user_unfollow(9777455) #Zendaya
+                client.user_unfollow(9777455) #Zendaya
                 unfollow += 1
                 print("[+] Unfollowing 9777455...")
                 sleep(2)
