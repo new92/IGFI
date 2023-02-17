d8d2e09db3fc5a6b4ff94651a89be88e942c9d47
new92 <94779840+new92@users.noreply.github.com>
2023-02-17 16:02:55 +0200

    🛸

--- InstagramFollowers/V2/mainV2.py
+++ InstagramFollowers/V2/mainV2.py
@@ -105,7 +105,7 @@ def ProgInfo():
         fsize = (os.stat(f)).st_size
     else:
         fsize = 0
-    stars = 16
+    stars = 17
     forks = 10
     print("[+] Author: "+str(author))
     print("[+] Github: @"+str(author))
