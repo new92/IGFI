69caac2fad0d88e566749006ae126b741e44ba16
new92 <94779840+new92@users.noreply.github.com>
2023-03-04 10:40:25 +0200

    Update mainV1.py

--- InstagramFollowers/V1/mainV1.py
+++ InstagramFollowers/V1/mainV1.py
@@ -4,7 +4,6 @@ Author: new92
 Github: @new92
 InstaFollowV1: Script for increasing the followers of an Instagram account
 
-
 *********IMPORTANT*********
 User's login credentials (such as: username, password) will not be stored or saved ! 
 Will be used only to increase the followers of the user's Instagram account
