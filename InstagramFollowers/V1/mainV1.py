7734e97c204f96f23d7309d078b2ff081831c6b7
new92 <94779840+new92@users.noreply.github.com>
2023-03-04 10:38:52 +0200

    Update mainV1.py

--- InstagramFollowers/V1/mainV1.py
+++ InstagramFollowers/V1/mainV1.py
@@ -110,7 +110,7 @@ def ProgInfo():
     print("[+] Natural language: "+lang)
     print("[+] Programming language(s) used: "+language)
     print("[+] Number of lines: "+str(lines))
-    print("[+] Script's name: "+str(name))
+    print("[+] Script's name: "+name)
     print("[+] File size: "+str(fsize)+" bytes")
     print("[+] Github repo stars: "+str(stars))
     print("[+] Github repo forks: "+str(forks))
