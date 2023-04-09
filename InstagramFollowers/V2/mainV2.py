8820b7a1f02f1e07f97711c33ed6850c80eafcbb
new92 <94779840+new92@users.noreply.github.com>
2023-04-09 12:30:54 +0300

    Update mainV2.py

--- InstagramFollowers/V2/mainV2.py
+++ InstagramFollowers/V2/mainV2.py
@@ -15,6 +15,7 @@ Will be used only to increase the followers of the user's Instagram account
 
 try:
     import sys
+    from time import sleep
     if sys.version_info[0] < 3:
         print("[!] Error ! This script requires Python version 3.X ! ")
         print("""[+] Instructions to download Python 3.x : 
@@ -28,13 +29,12 @@ try:
         quit(0)
     import platform
     from os import system
-    from time import sleep
     import os
     import instagrapi
     import requests
     import instaloader
     from datetime import date
-except ImportError as imp:
+except ImportError:
     print("[!] WARNING: Not all packages used in this script have been installed !")
     sleep(2)
     print("[+] Ignoring warning...")
