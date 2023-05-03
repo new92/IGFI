647329ef819096d298e53c37097a047055cd60ed
new92 <94779840+new92@users.noreply.github.com>
2023-05-04 00:15:25 +0300

    Update mainV1.py

--- InstagramFollowers/V1/mainV1.py
+++ InstagramFollowers/V1/mainV1.py
@@ -167,7 +167,7 @@ def nums():
     print("[2] Show program info and exit")
     print("[3] Keep log")
     print("[4] Clear log")
-    print("[5] Uninstall script")
+    print("[5] Uninstall IGFollowersIncreaser")
     print("[6] Exit")
 
 def main():
