e59521a2ecfb97ebcdb5951833824c3f66523e76
new92 <94779840+new92@users.noreply.github.com>
2023-09-17 18:49:58 +0300

    Update mainV1.py

--- InstagramFollowers/V1/mainV1.py
+++ InstagramFollowers/V1/mainV1.py
@@ -126,7 +126,8 @@ def ScriptInfo():
     with open('config.json') as config:
         conf = json.load(config)
     f = 'mainV1.py'
-    fsize = 0 if not os.path.exists(fpath(f)) else os.stat(fpath(f)).st_size
+    fp = os.path.exists(fpath(f)) if not fpath(f) == None else None
+    fsize = 0 if fp == None else os.stat(fpath(f)).st_size
     print(f"{YELLOW}[+] Author: {conf['author']}")
     print(f"{YELLOW}[+] Github: @{conf['author']}")
     print(f"{YELLOW}[+] License: {conf['lice']}")
